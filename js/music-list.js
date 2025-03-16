a4378bd0a8ea68c7835ff1215bfa65327c426993
ashwinr-10 <jashwinrao31@gmail.com>
2025-03-16 23:54:24 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+ 
+
+let allMusic = [
+  {
+    name: "SOS",
+    artist: "Avicii",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Castle of Glass",
+    artist: "Linkin Park",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Perfect",
+    artist: "One Direction",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Save your Tears",
+    artist: "Weeknd",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Viva la Vida",
+    artist: "Coldplay",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Attention",
+    artist: "Charlie Puth",
+    img: "music-6",
+    src: "music-6",
+  },
+];
